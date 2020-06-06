8c0233a69fda693bb162091da47af6745ce18eed
yxg1217 <694948161@qq.com>
2020-06-06 18:38:28 +0200

    model

--- model/customer.go
+++ model/customer.go
@@ -4,33 +4,47 @@ import (
 	"fmt"
 )
 
-//声明一个客户结构体,表示一个客户信息
+//Declare a customer structure, indicating a customer information
 
 type Customer struct {
 	Id int
 	Name string
-	Gender string
+	Sex string
 	Age int
 	Phone string
 	Email string
 }
 
+//Write a factory pattern that returns an instance of a customer
 //编写一个工厂模式,返回一个客户的实例
-func NewCustomer(id int, name string, gender string,
+func NewCustomer(id int, name string, sex string,
 	age int, phone string, email string ) Customer {
 		return Customer{
 			Id: id,
 			Name: name,
-			Gender: gender,
+			Sex: sex,
 			Age: age,
 			Phone: phone,
 			Email: email,
 		}
 	}
 
-	//返回用户的信息
+//Method for creating customer instance without Id 不带Id的创建客户实例方法
+func NewCustomer2(name string, sex string,
+	age int, phone string, email string ) Customer {
+		return Customer{
+			//Id: id,
+			Name: name,
+			Sex: sex,
+			Age: age,
+			Phone: phone,
+			Email: email,
+		}
+	}
+
+	//Return user information
 	func (this *Customer) GetInfo() string {
 		info := fmt.Printf("%v\t%v\t%v\t%v\t%v\t%v\t", this.Id, this.Name, 
-			this.Gender, this.Age, this.Phone, this.Email)
+			this.Sex, this.Age, this.Phone, this.Email)
 		return info
 	}
